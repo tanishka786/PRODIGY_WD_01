56525ae03980a7f9d675dc51ff0c974c24607958
tanishka786 <tanishkajdas@gmail.com>
2023-12-24 02:52:55 -0400

    change home page and add colour transition

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("The script is working");
+
+const navBar = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
+  if (this.scrollY >= 500 && this.scrollY < 899) {
+    console.log(this.scrollY);
+    console.log("above 500");
+    navBar.style.backgroundColor = "#256EFF";
+  } else if (this.scrollY >= 900 && this.scrollY < 1399) {
+    console.log(this.scrollY);
+    console.log("above 900");
+    navBar.style.backgroundColor = "#3DDC97";
+  } else if (this.scrollY >= 1400 && this.scrollY < 1749) {
+    console.log(this.scrollY);
+    console.log("above 1400");
+    navBar.style.backgroundColor = "#FCFCFC";
+  } else if (this.scrollY >= 1750) {
+    console.log(this.scrollY);
+    console.log("above 1800");
+    navBar.style.backgroundColor = "#FF495C";
+  }
+});
